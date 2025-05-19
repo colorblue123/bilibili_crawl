3b2062d500f48c86339d5090847eca563ffc2560
colorblue123 <159630792+colorblue123@users.noreply.github.com>
2025-05-19 20:35:50 +0800

    Add files via upload

--- download_model.py
+++ download_model.py
@@ -1,5 +1,8 @@
+import os
 from modelscope import snapshot_download
-snapshot_download('iic/speech_campplus_sv_zh-cn_16k-common')
-snapshot_download('iic/speech_fsmn_vad_zh-cn-16k-common-pytorch')
-snapshot_download('iic/punc_ct-transformer_zh-cn-common-vocab272727-pytorch')
-snapshot_download('iic/speech_seaco_paraformer_large_asr_nat-zh-cn-16k-common-vocab8404-pytorch')
+
+os.makedirs('./model')
+snapshot_download('iic/speech_campplus_sv_zh-cn_16k-common', cache_dir='./model')
+snapshot_download('iic/speech_fsmn_vad_zh-cn-16k-common-pytorch', cache_dir='./model')
+snapshot_download('iic/punc_ct-transformer_zh-cn-common-vocab272727-pytorch', cache_dir='./model')
+snapshot_download('iic/speech_seaco_paraformer_large_asr_nat-zh-cn-16k-common-vocab8404-pytorch', cache_dir='./model')
